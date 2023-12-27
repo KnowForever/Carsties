27dbcde7ed2dc263f33e5f6645cb7bdd8348b0af
Roman Cuscenco <r.cuscenco@gmail.com>
2023-12-27 19:37:47 +0200

    End of section 3

---
+++ src/SearchService/Data/DBInitializer.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.Services;
+using System.Text.Json;
+
+namespace SearchService.Data
+{
+    public class DBInitializer
+    {
+        public static async Task InitDb(WebApplication app)
+        {
+            await DB.InitAsync("SearchDb", MongoClientSettings
+                .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+            
+            await DB.Index<Item>()
+                .Key(i => i.Make, KeyType.Text)
+                .Key(i => i.Model, KeyType.Text)
+                .Key(i => i.Color, KeyType.Text)
+                .CreateAsync();
+
+            using var scope = app.Services.CreateScope();
+
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+            var items = await httpClient.GetItemsForSearchDb();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{items.Count} returned from the Auction service");
+
+            if (items.Count > 0)
+            {
+                await DB.SaveAsync(items);
+            }
+        }
+    }
+}
---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+    public class AuctionSvcHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            string AUCTIONS_SVC_ENDPOINT = $"{_config["AuctionServiceUrl"]}/api/auctions";
+
+            var lastUpdated = await DB.Find<Item, string>()
+                .Sort(i => i.Descending(_ => _.UpdatedAt))
+                .Project(i => i.UpdatedAt.ToString())
+                .ExecuteFirstAsync();
+
+            return await _httpClient.GetFromJsonAsync<List<Item>>($"{AUCTIONS_SVC_ENDPOINT}?date={lastUpdated}");
+        }
+    }
+}
